3458782310f56e75d4e00b19d9f4c77e4ac1c16f
Kethavath Siddu Kumar <kethavathsiddukumar@gmail.com>
2026-09-21 23:03:32 +0530

    Bind server to 0.0.0.0

--- ecommerce-app/server.js
+++ ecommerce-app/server.js
@@ -40,6 +40,6 @@ app.use('/api', (req, res) => {
   res.status(404).json({ error: 'Not found.' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0' , () => {
   console.log(`Store running at http://localhost:${PORT}`);
 });
